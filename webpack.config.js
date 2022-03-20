beab8c148b828cae32571fc040b440eacfcb94fb
gradywoodruff <jgradywoodruffiv@gmail.com>
2022-03-20 19:40:04 -0400

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,32 @@
 const path = require("path")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
   mode: "production",
   entry: "./src/scripts/main.js",
   output: {
-    filename: "script.js",
-    path: path.resolve(__dirname, "plugin")
-  }
+    filename: "script.min.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./plugin/manifest.json",
+          to: "manifest.json"
+        }
+      ]
+    })
+  ]
 }
